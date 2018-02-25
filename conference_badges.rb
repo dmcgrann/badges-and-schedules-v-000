6ea860cb8463b7958887214b2d2c10cae3f19100
David McGrann <dpmcgrann@gmail.com>
2018-02-25 17:33:20 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,5 +3,6 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
+  attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   attendees.map {|name| puts "Hello, my name is #{name}."}
 end
