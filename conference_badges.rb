65807c647aaf5995dc7fcc0e23ca028168106d6d
David McGrann <dpmcgrann@gmail.com>
2018-02-25 17:42:34 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,3 +5,11 @@ end
 def batch_badge_creator(attendees)
   attendees.map {|name| "Hello, my name is #{name}."}
 end
+
+def assign_rooms(attendees)
+  room = 1
+  attendees. each do |name|
+    puts "Hello, #{name}! You'll be assigned to room #{room}."
+    room += 1
+  end
+end
